68e88e67ed0de1c7fa88e47e40bc351ca44ccb28
Renê Soares <reenerochase@gmail.com>
2018-01-20 10:50:08 -0200

    Arquivo de exemplo const

---
+++ hoisting-const-let/const.js
@@ -0,0 +1,15 @@
+const MY_UF = 'PR';
+
+MY_UF = 'SE';
+
+console.log('Meu estado é ' + MY_UF);
+
+const MY_UF = 'RS';
+
+var MY_UF = 'SP';
+
+let MY_UF = 'BA';
+
+const estados = {'uf' : 'PR'};
+
+estados.uf = 'SE';
